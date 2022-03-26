fcb8e85a0d5673992ba58025888fe655f5a33ef8
woojush <tlsdntjr1121@naver.com>
2022-03-26 17:10:18 +0900

    first

---
+++ src/main/java/com/swj/exam/JpaBoard/JpaBoardApplication.java
@@ -0,0 +1,13 @@
+package com.swj.exam.JpaBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swj/exam/JpaBoard/JpaBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swj.exam.JpaBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
